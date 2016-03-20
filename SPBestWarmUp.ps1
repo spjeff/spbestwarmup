8d24552c9e4a58fc5184b353f9d6decf77b3fe02
Jeff Jones <spjeff@spjeff.com>
2016-03-20 15:34:32 -0500

    Code
    * removed IE GUI
    * web request only
    * install farm wide (multi-server SCHTASKS /S)
    * moved Custom URLs to end

--- SPBestWarmUp.ps1
+++ SPBestWarmUp.ps1
@@ -8,10 +8,16 @@
     
 	Comments and suggestions always welcome!  Please, use the dicussions or issues panel at the project page.
 
+.PARAMETER url
+	Typing "SPBestWarmUp.ps1 -install" will create a local Task Scheduler job under credentials of the
+	current user.  Job runs every 60 minutes on the hour to help automatically populate cache.  
+	Keeps cache full even after IIS daily recycle, WSP deployment, reboot, or other system events.
+	
 .PARAMETER install
 	Typing "SPBestWarmUp.ps1 -install" will create a local Task Scheduler job under credentials of the
-	current user.  Job runs every 60 minutes on the hour to help automatically populate cache after nightly IIS recycle.
-
+	current user.  Job runs every 60 minutes on the hour to help automatically populate cache.  
+	Keeps cache full even after IIS daily recycle, WSP deployment, reboot, or other system events.
+	
 .EXAMPLE
 	.\SPBestWarmUp.ps1 -url "http://domainA.tld","http://domainB.tld"
 
@@ -20,14 +26,15 @@
 	.\SPBestWarmUp.ps1 -install
 
 .EXAMPLE
-	.\SPBestWarmUp.ps1 -wr
-    .\SPBestWarmUp.ps1 -webrequest
+    .\SPBestWarmUp.ps1 -f
+	.\SPBestWarmUp.ps1 -installfarm
+
 	
 .NOTES  
     File Name     : SPBestWarmUp.ps1
     Author        : Jeff Jones  - @spjeff
 					Hagen Deike - @hd_ka
-    Version       : 2.11
+    Version       : 2.2
 	Last Modified : 03-20-2016
 
 .LINK
@@ -42,13 +49,13 @@ param (
     [ValidatePattern("https?:\/\/\D+")]
     [string[]]$cmdurl,
 
-	[Parameter(Mandatory=$False, Position=1, ValueFromPipeline=$false, HelpMessage='Use -Install parameter to add script to the Windows Task Scheduler')]
+	[Parameter(Mandatory=$False, Position=1, ValueFromPipeline=$false, HelpMessage='Use -install -i parameter to add script to Windows Task Scheduler on local machine')]
 	[Alias("i")]
     [switch]$install,
-
-	[Parameter(Mandatory=$False, Position=2, ValueFromPipeline=$false, HelpMessage='Use -WebRequest parameter to load HTTP with Invoke-WebRequest instead of Internet Explorer GUI')]
-	[Alias("wr")]
-    [switch]$webrequest
+	
+	[Parameter(Mandatory=$False, Position=2, ValueFromPipeline=$false, HelpMessage='Use -installfarm -f parameter to add script to Windows Task Scheduler on all farm machines')]
+	[Alias("f")]
+    [switch]$installfarm
 )
 
 Function Installer() {
@@ -73,28 +80,30 @@ Function Installer() {
 		$pass = Read-Host "Enter password for $user "
 	}
 	
-	# Create Task
-	if ($webrequest) {$global:path += " -wr"}
-	$cmd = """PowerShell.exe -ExecutionPolicy Bypass '$global:path'"""
-	schtasks /create /tn "SPBestWarmUp" /ru $user /rp $pass /rl highest /sc daily /st 01:00 /ri 60 /du 24:00 /tr $cmd
-	Write-Output "  [OK]"
+	# Command
+	$cmd = """PowerShell.exe -ExecutionPolicy Bypass '$global:path -webrequest'"""
+	
+	# Target machines
+	if ($installfarm) {
+		# Create farm wide on remote machines
+		foreach ($srv in (Get-SPServer |? {$_.Role -ne "Invalid"})) {
+			$machines += $srv.Address
+		}
+	} else {
+		# Create local on current machine
+		$machines = "localhost"
+	}
+	$machines |% {
+		Write-Output "SCHTASKS CREATE on $_"
+		schtasks /s $_ /create /tn "SPBestWarmUp" /ru $user /rp $pass /rl highest /sc daily /st 01:00 /ri 60 /du 24:00 /tr $cmd
+		Write-Host "  [OK]" -Fore Green
+	}
 }
 
 Function WarmUp() {
 	# Load plugin
 	Add-PSSnapIn Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
-	$was = Get-SPWebApplication -IncludeCentralAdministration
-	$was |? {$_.IsAdministrationWebApplication -eq $true} |% {$caTitle = Get-SPWeb $_.Url | Select Title}
-	
-	# Open Internet Explorer
-	if (!$webrequest) {
-		Write-Output "Opening Internet Explorer..."
-		$global:ie = New-Object -Com "InternetExplorer.Application"
-		$global:ie.Navigate("about:blank")
-		$global:ie.Visible = $true
-		$global:ieproc = (Get-Process -Name iexplore)|? {$_.MainWindowHandle -eq $global:ie.HWND}
-	}
-	
+
     # Warm up CMD parameter URLs
     $cmdurl |% {NavigateTo $_}
 
@@ -135,105 +144,28 @@ Function WarmUp() {
 	# Warm up Topology
 	NavigateTo "http://localhost:32843/Topology/topology.svc"
 	
-	# Add your own URLs below.  Looks at Central Admin Site Title for full lifecycle support in a single script file.
-	switch -Wildcard ($caTitle) {
-		"*PROD*" {
-			#NavigateTo "http://portal/popularPage.aspx"
-			#NavigateTo "http://portal/popularPage2.aspx"
-			#NavigateTo "http://portal/popularPage3.aspx
-		}
-		"*TEST*" {
-			#NavigateTo "http://portal/popularPage.aspx"
-			#NavigateTo "http://portal/popularPage2.aspx"
-			#NavigateTo "http://portal/popularPage3.aspx
-		}
-		"*DEV*" {
-			#NavigateTo "http://portal/popularPage.aspx"
-			#NavigateTo "http://portal/popularPage2.aspx"
-			#NavigateTo "http://portal/popularPage3.aspx
-		}
-		default {
-			#NavigateTo "http://portal/popularPage.aspx"
-			#NavigateTo "http://portal/popularPage2.aspx"
-			#NavigateTo "http://portal/popularPage3.aspx
-		}
-	}
-	
 	# Warm up Host Name Site Collections (HNSC)
 	Write-Output "Opening Host Name Site Collections (HNSC)..."
 	$hnsc = Get-SPSite -Limit All |? {$_.HostHeaderIsSiteName -eq $true} | Select Url
 	foreach ($sc in $hnsc) {
 		NavigateTo $sc.Url
 	}
-	
-	# Cleanup
-	if (!$webrequest) {
-		# Close IE window
-		if ($global:ie) {
-			Write-Output "Closing IE"
-			$global:ie.Quit()
-		}
-		$global:ieproc | Stop-Process -Force -ErrorAction SilentlyContinue
-		
-		# Clean Temporary Files
-		Remove-item "$env:systemroot\system32\config\systemprofile\appdata\local\microsoft\Windows\temporary internet files\content.ie5\*.*" -Recurse -ErrorAction SilentlyContinue
-		Remove-item "$env:systemroot\syswow64\config\systemprofile\appdata\local\microsoft\Windows\temporary internet files\content.ie5\*.*" -Recurse -ErrorAction SilentlyContinue
-	}
 }
 
-Function NavigateTo([string] $url, [int] $delayTime = 500) {
+Function NavigateTo([string] $url) {
     if ($url.ToUpper().StartsWith("HTTP")) {
         Write-Host "  $url" -NoNewLine
-	    if ($webrequest) {
-            # WebRequest command line    
-            try {
-				$wr = Invoke-WebRequest -Uri $url -UseBasicParsing -UseDefaultCredentials -TimeoutSec 120
-				FetchResources $url "Images" $wr.Images
-				FetchResources $url "Scripts" $wr.Scripts
-				
-            } catch {}
-        } else {
-		    # Internet Explorer
-		    try {
-			    $global:ie.Navigate($url)
-		    } catch {
-			    try {
-				    $pid = $global:ieproc.id
-			    } catch {}
-			    Write-Output "  IE not responding.  Closing process ID $pid"
-			    $global:ie.Quit()
-			    $global:ieproc | Stop-Process -Force -ErrorAction SilentlyContinue
-			    $global:ie = New-Object -Com "InternetExplorer.Application"
-			    $global:ie.Navigate("about:blank")
-			    $global:ie.Visible = $true
-			    $global:ieproc = (Get-Process -Name iexplore)|? {$_.MainWindowHandle -eq $global:ie.HWND}
-		    }
-		    IEWaitForPage $delayTime
-	    }
+		# WebRequest command line    
+		try {
+			$wr = Invoke-WebRequest -Uri $url -UseBasicParsing -UseDefaultCredentials -TimeoutSec 120
+			FetchResources $url $wr.Images
+			FetchResources $url $wr.Scripts
+		} catch {}
 		Write-Host "."
     }
 }
 
-Function IEWaitForPage([int] $delayTime = 500) {
-	# Wait for current page to finish loading
-	$loaded = $false
-	$loop = 0
-	$maxLoop = 20
-	while ($loaded -eq $false) {
-		$loop++
-		if ($loop -gt $maxLoop) {
-			$loaded = $true
-		}
-		[System.Threading.Thread]::Sleep($delayTime) 
-		# If the browser is not busy, the page is loaded
-		if (-not $global:ie.Busy)
-		{
-			$loaded = $true
-		}
-	}
-}
-
-Function FetchResources($baseUrl, $type, $resources) {
+Function FetchResources($baseUrl, $resources) {
 	# Download additional HTTP files
 	[uri]$uri = $baseUrl
 	$rootUrl = $uri.Scheme + "://" + $uri.Authority
@@ -244,27 +176,28 @@ Function FetchResources($baseUrl, $type, $resources) {
 		# Support both abosolute and relative URLs
 		$resUrl  = $res.src
 		if ($resUrl -contains "HTTP") {
-			$scriptUrl = $res.src
+			$fetchUrl = $res.src
 		} else {
 			if (!$resUrl.StartsWith("/")) {
 				$resUrl = "/" + $resUrl
 			}
-			$scriptUrl = $rootUrl + $resUrl
+			$fetchUrl = $rootUrl + $resUrl
 		}
 
 		# Progress
-		Write-Progress -Activity "Opening " -status $scriptUrl -Id 2 -ParentId 1 -PercentComplete (($counter/$resources.Count)*100)
+		Write-Progress -Activity "Opening " -Status $fetchUrl -PercentComplete (($counter/$resources.Count)*100)
 		$counter++
 		
 		# Execute
-		$scriptReturn = Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $scriptUrl -TimeoutSec 120
+		$scriptReturn = Invoke-WebRequest -UseDefaultCredentials -UseBasicParsing -Uri $fetchUrl -TimeoutSec 120
 		Write-Host "." -NoNewLine
 	}
 }
 
 Function ShowW3WP() {
-	$mb = [Math]::Round((Get-Process w3wp | Select pm | Measure pm -Sum).Sum/1MB)
-	Write-Warning "Total W3WP = $mb MB"
+	# Total memory used by IIS worker processes
+	$mb = [Math]::Round((Get-Process W3WP -ErrorAction SilentlyContinue | Select pm | Measure pm -Sum).Sum/1MB)
+	Write-Host "Total W3WP = $mb MB" -Fore Green
 }
 
 # Main
@@ -276,17 +209,42 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
     break
 } else {
-    # Warm up
+    # Task Scheduler support
     $global:path = $MyInvocation.MyCommand.Path
     $tasks = schtasks /query /fo csv | ConvertFrom-Csv
     $spb = $tasks |? {$_.TaskName -eq "\SPBestWarmUp"}
     if (!$spb -and !$install) {
 	    Write-Warning "Tip: to install on Task Scheduler run the command ""SPBestWarmUp.ps1 -install"""
     }
-    if ($install) {
-	    Installer
+    if ($install || $installfarm) {
+		Installer
     }
+	
+	# Core
 	ShowW3WP
     WarmUp
 	ShowW3WP
+	
+	# Custom URLs - Add your own below
+	# Looks at Central Admin Site Title to support many farms with a single script
+	(Get-SPWebApplication -IncludeCentralAdministration) |? {$_.IsAdministrationWebApplication -eq $true} |% {
+		$caTitle = Get-SPWeb $_.Url | Select Title
+	}
+	switch -Wildcard ($caTitle) {
+		"*PROD*" {
+			#NavigateTo "http://portal/popularPage.aspx"
+			#NavigateTo "http://portal/popularPage2.aspx"
+			#NavigateTo "http://portal/popularPage3.aspx
+		}
+		"*TEST*" {
+			#NavigateTo "http://portal/popularPage.aspx"
+			#NavigateTo "http://portal/popularPage2.aspx"
+			#NavigateTo "http://portal/popularPage3.aspx
+		}
+		default {
+			#NavigateTo "http://portal/popularPage.aspx"
+			#NavigateTo "http://portal/popularPage2.aspx"
+			#NavigateTo "http://portal/popularPage3.aspx
+		}
+	}
 }
